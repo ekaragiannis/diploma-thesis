689e823957afca3cc3f85b88056deffb620e9e2e
Efthymios Karagiannis <efthimios.karagiannis@outlook.com>
2025-05-04 22:36:29 +0300

    create hourly data hypertable with continuous aggregate and policy

---
+++ sql/insert-test-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO sensors_data (device_id, time, temperature)
+SELECT
+    FLOOR(RANDOM() * 3 + 1)::INT AS device_id,
+    DATE_TRUNC('day', NOW()) + INTERVAL '1 day' * RANDOM() AS time,
+    ROUND((RANDOM() * 43)::numeric, 2)::float AS temperature
+FROM generate_series(1, 1000);
---
+++ sql/timescaledb-init.sql
@@ -0,0 +1,27 @@
+CREATE PUBLICATION dbz_publication FOR ALL TABLES WITH (publish = 'insert, update');
+
+CREATE TABLE sensors_data (
+    device_id INT NOT NULL,
+    time TIMESTAMPTZ NOT NULL,
+    temperature FLOAT,
+    PRIMARY KEY (device_id, time)
+);
+
+SELECT create_hypertable('sensors_data', 'time');
+
+CREATE MATERIALIZED VIEW hourly_data
+WITH (timescaledb.continuous) AS
+SELECT
+    device_id,
+    time_bucket('1 hour', time) AS bucket,
+    AVG(temperature) AS avg_temp,
+    MAX(temperature) AS max_temp,
+    MIN(temperature) AS min_temp
+FROM sensors_data
+GROUP BY device_id, bucket;
+
+SELECT add_continuous_aggregate_policy('hourly_data',
+    start_offset => INTERVAL '3 hours',
+    end_offset   => INTERVAL '15 minutes',
+    schedule_interval => INTERVAL '10 minutes');
+
