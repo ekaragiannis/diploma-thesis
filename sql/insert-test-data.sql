2351e3ce8e13a26f0e9f96ca0c06b1368fd31d2b
Efthymios Karagiannis <efthimios.karagiannis@outlook.com>
2025-05-13 23:21:43 +0300

    Update redis stored value to include daily data

--- sql/insert-test-data.sql
+++ sql/insert-test-data.sql
@@ -1,6 +1,6 @@
 INSERT INTO sensors_data (device_id, time, temperature)
 SELECT
     FLOOR(RANDOM() * 3 + 1)::INT AS device_id,
-    DATE_TRUNC('day', NOW()) + INTERVAL '1 day' * RANDOM() AS time,
+    DATE_TRUNC('day', NOW()) - INTERVAL '2 day' * RANDOM() AS time,
     ROUND((RANDOM() * 43)::numeric, 2)::float AS temperature
-FROM generate_series(1, 1000);
+FROM generate_series(1, 3000);
