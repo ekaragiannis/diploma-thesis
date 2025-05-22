03002616ab9b76b71158761225723f079c9f6388
Efthymios Karagiannis <efthimios.karagiannis@outlook.com>
2025-05-23 02:18:48 +0300

    fix redis key bug; auto register schema for sensors metrics kafka topic

--- ksql/init-ksql.sh
+++ ksql/init-ksql.sh
@@ -6,8 +6,8 @@ INPUT_TOPIC="db.public.HourlySummary"
 echo "Waiting for schema for topic: $INPUT_TOPIC..."
 echo "$KAFKA_SCHEMA_REGISTRY/subjects/${INPUT_TOPIC}-value/versions/latest"
 
-# Wait for the value schema to be registered
-until curl -s "$KAFKA_SCHEMA_REGISTRY/subjects/${INPUT_TOPIC}-value/versions/latest" > /dev/null; do
+# Wait for the value schema to be registered by 
+until curl -s -f "$KAFKA_SCHEMA_REGISTRY/subjects/${INPUT_TOPIC}-value/versions/latest" > /dev/null; do
   echo "Schema not yet available. Retrying..."
   sleep 10
 done
@@ -27,7 +27,31 @@ curl -X POST "$KAFKA_KSQL_SERVER/ksql" \
      -H "Content-Type: application/vnd.ksql.v1+json; charset=utf-8" \
      -d @"-"<<EOF
 { 
-  "ksql": "CREATE TABLE RedisHourlySummary WITH ( kafka_topic='redis.HourlySummary', value_format='AVRO' ) AS SELECT after->id AS id, AS_MAP( COLLECT_LIST( FORMAT_TIMESTAMP( PARSE_TIMESTAMP(after->hour, 'yyyy-MM-dd''T''HH:mm:ss.SSSSSS''Z'''), 'HH' ) ), COLLECT_LIST(after->hour_total) ) AS \"data\" FROM DbHourlySummary WINDOW TUMBLING (SIZE 1 DAY) GROUP BY after->id EMIT CHANGES;",
+  "ksql": "CREATE TABLE RedisHourlySummary WITH (
+    kafka_topic='redis.HourlySummary',
+    value_format='AVRO'
+  ) AS
+  SELECT
+    after->id AS id,
+    AS_MAP(
+      COLLECT_LIST(
+        FORMAT_TIMESTAMP(
+          PARSE_TIMESTAMP(after->hour, 'yyyy-MM-dd''T''HH:mm:ss.SSSSSS''Z'''),
+          'HH'
+        )
+      ),
+      COLLECT_LIST(after->hour_total)
+    ) AS \"data\"
+  FROM DbHourlySummary
+  WHERE FORMAT_TIMESTAMP(
+    PARSE_TIMESTAMP(after->hour, 'yyyy-MM-dd''T''HH:mm:ss.SSSSSS''Z'''), 
+    'yyyy-MM-dd'
+  ) = FORMAT_TIMESTAMP(
+    FROM_UNIXTIME(UNIX_TIMESTAMP()), 
+    'yyyy-MM-dd'
+  )
+  GROUP BY after->id
+  EMIT CHANGES;",
   "streamsProperties": {}
 }
 EOF
